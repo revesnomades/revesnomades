e9856525a25ce559b7d6e9f789f1043158e5023e
rêves-nomades <contact@revesnomades.com>
2026-02-02 00:27:37 +0100

    modif 01/02 5

--- assets/js/supabaseClient.js
+++ assets/js/supabaseClient.js
@@ -1,16 +1,16 @@
 // assets/js/supabaseClient.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://qxrxwgrgjvesevjkjmry.supabase.co";
+export const SUPABASE_URL = "https://qxrxwgrgjvesevjkjmry.supabase.co";
 
-// ⚠️ Mets ici ta clé ANON publique (Supabase > Settings > API > anon public)
-// Elle commence souvent par "eyJ..."
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4cnh3Z3JnanZlc2V2amtqbXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNzE4MTIsImV4cCI6MjA4Mjk0NzgxMn0.WQCWMfqGbhbdnYdkcpBAarVJBBstSFAK3_YYn5uNbKI";
+// ✅ Clé ANON publique (Supabase > Settings > API > anon public)
+export const SUPABASE_ANON_KEY =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF4cnh3Z3JnanZlc2V2amtqbXJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNzE4MTIsImV4cCI6MjA4Mjk0NzgxMn0.WQCWMfqGbhbdnYdkcpBAarVJBBstSFAK3_YYn5uNbKI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
     persistSession: true,
     autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
+    detectSessionInUrl: true,
+  },
 });
